5f5c2877346130e9a0705d9c672bf08cd743412b
Atharva3231 <129871402+Atharva3231@users.noreply.github.com>
2024-08-08 23:01:59 +0530

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    DRAFT = 'DF'
+    PUBLISHED = 'PB'
+    STATUS_CHOICES = [
+        (DRAFT, 'Draft'),
+        (PUBLISHED, 'Published'),
+    ]
+
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default=DRAFT)
+
+    def __str__(self):
+        return self.title
 
